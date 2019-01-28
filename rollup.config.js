83e881c4e5a7431eeccc2d7c24b8bbe7c6b2fd9e
Austin McGee <>
2019-01-28 16:57:54 +0000

    Bundle babel runtime in cjs bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 // rollup.config.js
 import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 
 export default {
@@ -10,7 +11,11 @@ export default {
   },
   plugins: [
     resolve(),
+    commonjs({
+      include: 'node_modules/**'
+    }),
     babel({
+      runtimeHelpers: true,
       exclude: "node_modules/**" // only transpile our source code
     })
   ],
